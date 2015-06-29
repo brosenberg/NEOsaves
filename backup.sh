e4795dfe765e37ee8033f0e5d78f71a37c6bf0b7
brosenberg <brosenberg@pistoncloud.com>
2015-06-28 18:24:13 -0700

    Stop using tar. Make list more informative. Create description.

--- backup.sh
+++ backup.sh
@@ -3,16 +3,19 @@
 set -e
 set -o pipefail
 
+
 # Change this to your appropriate directory
 SAVE_DIR="/Users/brosenberg/Library/Preferences/Macromedia/Flash Player/#SharedObjects/3P2ZYSJC/localhost"
 TS=$(stat -f %m "$SAVE_DIR/nsSGv1.sol")
 OUTPUT_DIR="NEOsaves/$TS"
 
-mkdir -p ~/$OUTPUT_DIR 2>/dev/null
-cd "$SAVE_DIR"
-tar cf saves_$TS.tar ./*
-mv saves_$TS.tar ~/$OUTPUT_DIR
 
-echo "Contents of $(ls ~/$OUTPUT_DIR):"
-echo
-tar tf ~/$OUTPUT_DIR/saves_$TS.tar
+
+mkdir -p ~/$OUTPUT_DIR 2>/dev/null
+cp "$SAVE_DIR/"*.sol ~/$OUTPUT_DIR
+if [ ! -z "$1" ]; then
+    echo "$1" >> ~/$OUTPUT_DIR/desc
+fi
+
+echo "Contents of $(ls -d ~/$OUTPUT_DIR):"
+ls ~/$OUTPUT_DIR
--- list.sh
+++ list.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-for d in $(find . -type d -maxdepth 1 -mindepth 1); do
+for d in $(find . -type d -maxdepth 1 -mindepth 1|sed /\.git/d); do
+    TS=$(date -r $(sed 's/^\.\///' <<< $d) '+%d %b %Y %T')
     if [ -f $d/desc ]; then
-        echo "$d: $(cat $d/desc)"
+        DESC=$(cat $d/desc)
+    else
+        DESC="[No Description]"
     fi
+    echo "$d: ($TS) $DESC"
 done
 
